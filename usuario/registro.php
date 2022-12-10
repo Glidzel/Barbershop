b00b0dec708e97e74d0d8a76665599075919ee22
Glidzel <eduardoquiroz20@outlook.com>
2022-12-10 12:35:03 -0500

    aa

--- usuario/registro.php
+++ usuario/registro.php
@@ -1,21 +1,22 @@
-<?php 
- require './php/conexion.php';
+<?php
 
- $message = '';
+  require 'database.php';
 
- if (!empty($_POST['email']) && !empty($_POST['password'])) {
-   $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
-   $stmt = $conn->prepare($sql);
-   $stmt->bindParam(':email', $_POST['email']);
-   $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-   $stmt->bindParam(':password', $password);
+  $message = '';
 
-   if ($stmt->execute()) {
-     $message = 'Successfully created new user';
-   } else {
-     $message = 'Sorry there must have been an issue creating your account';
-   }
- }
+  if (!empty($_POST['email']) && !empty($_POST['password'])) {
+    $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':email', $_POST['email']);
+    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+    $stmt->bindParam(':password', $password);
+
+    if ($stmt->execute()) {
+      $message = 'Successfully created new user';
+    } else {
+      $message = 'Sorry there must have been an issue creating your account';
+    }
+  }
 ?>
 
 <!DOCTYPE html>
@@ -70,7 +71,6 @@
             
         </form>
     </div>
-    <script src="./js/script.js"></script>
 </body>
 
 </html>
